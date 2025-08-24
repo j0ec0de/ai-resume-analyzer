101488e16c7fa07b64f767b6c8d39a3c7e017428
j0ec0de <joelanto386@gmail.com>
2025-08-24 18:22:27 +0530

    Authentication and puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,10 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "constants";
 import ResumeCard from "~/components/ResumeCard";
-
+import { useLocation, useNavigate } from 'react-router'
+import React, { useEffect } from 'react'
+import { usePuterStore } from '~/lib/puter'
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,8 +14,19 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
+
+
     <section className="main-section">
       <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
@@ -28,8 +41,6 @@ export default function Home() {
     )}
 
     </section>
-
-
 
   </main>;
 }
